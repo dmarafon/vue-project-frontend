2b8088cdb8f5f8cc5a520c64badc59a58d6f46af
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-25 21:16:16 +0200

    Removing test Button

--- src/components/ButtonTest.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-export default { name: "ButtonTest", props: { buttonName: { type: String } } };
-</script>
-
-<template>
-  <div>
-    <button>{{ buttonName }}</button>
-  </div>
-</template>
